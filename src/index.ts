26894b98d23c453377f7c94464cb2cb16cd819ad
Marcelo Miyachi <marcelo.m1811@gmail.com>
2022-08-06 16:57:43 -0300

    refactor(users): add User not found

--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ let users: User[] = [
 ];
 
 app.get("/", (_req, res) => {
-  res.status(200).send({
+  return res.status(200).send({
     method: "express clone v1.0",
   });
 });
@@ -20,7 +20,14 @@ app.get("/", (_req, res) => {
 app.get("/users/:userId", (req, res) => {
   const { userId } = req.params;
   const user = users.find((user) => user.id === Number(userId));
-  res.status(200).send(user);
+
+  if (!user) {
+    return res.status(404).send({
+      message: "User not found",
+    });
+  }
+
+  return res.status(200).send(user);
 });
 
 app.get("/users", (req, res) => {
@@ -28,7 +35,7 @@ app.get("/users", (req, res) => {
   const filteredUsers = users.filter((user) =>
     user.name.startsWith(name || "")
   );
-  res.status(200).send(filteredUsers);
+  return res.status(200).send(filteredUsers);
 });
 
 app.post("/users", (req, res) => {
@@ -39,34 +46,40 @@ app.post("/users", (req, res) => {
   };
   users.push(newUser);
 
-  res.status(201).send(newUser);
+  return res.status(201).send(newUser);
 });
 
 app.patch("/users/:userId", (req, res) => {
   const { userId } = req.params;
   const userData = req.body as Omit<User, "id">;
 
-  let updatedUser;
-  users = users.map((user) => {
-    if (user.id === Number(userId)) {
-      updatedUser = {
-        ...user,
-        ...userData,
-      };
-      return updatedUser;
-    }
-    return user;
-  });
+  const user = users.find((user) => user.id === Number(userId));
+  if (!user) {
+    return res.status(404).send({
+      message: "User not found",
+    });
+  }
 
-  res.status(200).send(updatedUser);
+  const updatedUser = {
+    ...user,
+    ...userData,
+  };
+  Object.assign(user, updatedUser);
+  return res.status(200).send(updatedUser);
 });
 
 app.delete("/users/:userId", (req, res) => {
-  const userToDelete = users.find(
+  const deletedUser = users.find(
     (user) => user.id === Number(req.params.userId)
   );
+  if (!deletedUser) {
+    return res.status(404).send({
+      message: "User not found",
+    });
+  }
+  
   users = users.filter((user) => user.id !== Number(req.params.userId));
-  res.status(200).send(userToDelete);
+  return res.status(200).send(deletedUser);
 });
 
 app.listen(3000, () => {
